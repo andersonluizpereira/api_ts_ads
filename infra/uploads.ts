7cd948af76dc1c12e21515d44b5f13697361c3f3
Anderson <andersononus@hotmail.com>
2020-01-17 22:02:33 -0300

    ajustando upload de arquivos

--- infra/uploads.ts
+++ infra/uploads.ts
@@ -1,11 +1,12 @@
 import * as multer from "multer";
+import * as path from "path";
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, path.resolve(__dirname,'..','..', 'uploads'));
   },
   filename: function(req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, `${file.originalname}-${Date.now()}${path.extname(file.originalname)}`);
   }
 });
 
